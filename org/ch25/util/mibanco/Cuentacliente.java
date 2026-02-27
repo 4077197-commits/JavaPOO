430a930e4c6c0ce53ce09ed922f45d77cce5b290
4077197-commits <4077197@alu.murciaeduca.es>
2026-02-27 12:51:28 +0100

    Refaactorizo la clase cuenta

--- org/ch25/util/mibanco/Cuentacliente.java
+++ org/ch25/util/mibanco/Cuentacliente.java
@@ -1,41 +1,42 @@
-package org.ch25.util.banco;
+package org.ch25.util.mibanco;
 
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
 import org.ch25.util.Imprimible;
 
-public abstract class Cuenta implements Imprimible{
+public abstract class Cuentacliente implements Imprimible{
 	//Los números de cuenta del banco iniciarán después de 1000
 	private static int consecutivo=1000; 
 	protected double saldo;
 	private int numeroCuenta; 
 	private String numeroCliente;
 	private Date fechaApertura;
-	private String nombreCliente;
-    /**
-     * 
-     * @param saldo double - Saldo inicial de la cuenta
-     * @param numeroCliente String  - Número del cliente, si se establece en 0 se genera uno aleatorio 
-     * @param fechaApertura  java.util.Date - Fecha de apertura de la cuenta
-     * @param nombreBanco String - Nombre del banco 
-    */
-	
-	public Cuenta(double saldo, String numeroCliente, Date fechaApertura, 
-													String nombreCliente) {
+	private String nomCli;
+
+	public Cuentacliente(String nomCli) {
+		this.nomCli = nomCli;
+	}
+
+	public Cuentacliente(double saldo, String numeroCliente, Date fechaApertura,
+						 String nomCli) {
 	    this.saldo = saldo;
-	    Cuenta.consecutivo++;
-	    this.numeroCuenta = Cuenta.consecutivo;
-	    this.setNombreCliente(nombreCliente);
+	    Cuentacliente.consecutivo++;
+	    this.numeroCuenta = Cuentacliente.consecutivo;
+	    this.setNombreCliente(nomCli);
 		this.numeroCliente = numeroCliente;
 		if (numeroCliente.equals("0")){ //Se genera un número de cliente aleatorio
-			this.numeroCliente =Integer.toString(Math.abs(new Random().nextInt()));
+			this.numeroCliente =Integer.toString(getAbs());
 	    }//if numCliente.equals
 		this.fechaApertura = fechaApertura;
 	}//constructor
-	public Cuenta(double saldo) {
+
+	private static int getAbs() {
+		return Math.abs(new Random().nextInt());
+	}
+
+	public Cuentacliente(double saldo) {
 		this.saldo=saldo;
 	}//constructor
 	//Los métodos abstract deben ser implementados en la clase que hereda
@@ -65,21 +66,20 @@ public abstract class Cuenta implements Imprimible{
 		return format.format(fechaApertura);
 	}//getFechaApertura
 
-	protected String getNombreCliente() {
-		return nombreCliente;
-	} //getNombreCliente
-
-	protected void setNombreCliente(String nombreCliente) {
-		this.nombreCliente = nombreCliente.toUpperCase();
-	}//setNombreCliente
-
 	public String toString() {
 		return "Saldo=" + saldo + ", Número de Cuenta=" + getNumeroCuenta()
 				+ ", Número del Cliente=" + getNumeroCliente()
 				+ ", Fecha de Apertura=" + getFechaApertura()
 				+ ", Nombre del Cliente=" + getNombreCliente();
 	}//toString
- 	
+
+	public String getNomCli() {
+		return nomCli;
+	}
+
+	public void setNomCli(String nomCli) {
+		this.nomCli = nomCli;
+	}
 }//class
 
 
